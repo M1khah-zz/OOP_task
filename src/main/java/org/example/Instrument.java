18a938f29e018a0ad146ba4ca46da249a4ad3b2d
m1khah <alexeyenko92@gmail.com>
2020-04-19 18:45:24 +0300

    Added frame for Music Store

---
+++ src/main/java/org/example/Instrument.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import static java.lang.System.out;
+
+public abstract class Instrument {
+    private Color color;
+    private float price;
+    private int inCount;
+
+    public abstract void sound();
+
+    Instrument(float price) {
+        setPrice(price);
+    }
+
+    float getPrice() {
+        return price;
+    }
+
+    void setPrice(float price) {
+        this.price = price;
+    }
+
+    void setColor(Color color) {
+        this.color = color;
+    }
+
+    void sell(String price) {
+        try {
+            out.println("You can sell this instrument for " + price);
+            inCount--;
+        } catch (Exception e) {
+            e.printStackTrace();
+            out.println("Sorry, customer is out of money!");
+        }
+    }
+
+
+    public void play(boolean isBroken) throws InstrumentIsBrokenException {
+        if (isBroken) {
+            throw new InstrumentIsBrokenException("Your instrument is broken - you can't play on it");
+        }
+        out.println("Adagio");
+    }
+
+
+}
---
+++ src/main/java/org/example/InstrumentIsBrokenException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class InstrumentIsBrokenException extends  Exception {
+    InstrumentIsBrokenException(String message) {
+        super(message);
+    }
+}
