5180e82ca933251d946ece5b5fd4fd3e87ce2bb2
m1khah <alexeyenko92@gmail.com>
2020-04-19 19:23:10 +0300

    Added Accordion implementation

--- src/main/java/org/example/Accordion.java
+++ src/main/java/org/example/Accordion.java
@@ -1,4 +1,16 @@
 package org.example;
 
-public class Accordion {
+public class Accordion extends Instrument implements Keyboard {
+
+    Accordion(float price) {
+        super(price);
+    }
+
+    public void sound() {
+        System.out.println("Bayan");
+    }
+
+    public boolean keyboard() {
+        return true;
+    }
 }
